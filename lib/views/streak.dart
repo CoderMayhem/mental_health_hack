eb07f320041f792a11e5b84821d27462473d0ae9
CoderMayhem <namanmishra1900@gmail.com>
2021-05-23 11:22:45 +0530

    feat: adds resest option to streak

--- lib/views/streak.dart
+++ lib/views/streak.dart
@@ -116,34 +116,50 @@ class _StreakState extends State<Streak> {
                             ),
                           )
                         ),
-                        Padding(
-                            padding: EdgeInsets.all(20),
-                          child: GestureDetector(
-                            onTap: onTapIncrement,
-                            child: Container(
-                              padding: EdgeInsets.symmetric(vertical: 20, horizontal: 30),
-                              decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(20),
-                                gradient: LinearGradient(
-                                  colors: [
-                                    const Color(0xFF3366FF),
-                                    const Color(0xFF00CCFF),
-                                  ],
-                                  begin: const FractionalOffset(0.0, 0.0),
-                                  end: const FractionalOffset(1.0, 0.0),
-                                  stops: [0.0, 1.0],
-                                  tileMode: TileMode.clamp,
-                                ),
-                              ),
-                              child: Text(
-                                '+1',
-                                style: TextStyle(
-                                  fontSize: ScreenUtil().setSp(40),
-                                  color: Colors.white,
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            Padding(
+                                padding: EdgeInsets.all(20),
+                              child: GestureDetector(
+                                onTap: onTapIncrement,
+                                child: Container(
+                                  padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(20),
+                                    color: Colors.green[800],
+                                  ),
+                                  child: Text(
+                                    'Add',
+                                    style: TextStyle(
+                                      fontSize: ScreenUtil().setSp(20),
+                                      color: Colors.white,
+                                    ),
+                                  ),
                                 ),
                               ),
                             ),
-                          ),
+                            Padding(
+                              padding: EdgeInsets.all(20),
+                              child: GestureDetector(
+                                onTap: _showMyDialog,
+                                child: Container(
+                                  padding: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(20),
+                                    color: Colors.red[800],
+                                  ),
+                                  child: Text(
+                                    'Reset',
+                                    style: TextStyle(
+                                      fontSize: ScreenUtil().setSp(20),
+                                      color: Colors.white,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
                         ),
                         Center(
                           child: Text(
@@ -177,6 +193,49 @@ class _StreakState extends State<Streak> {
       });
     });
     hideProgress();
+  }
+
+  void onTapReset() async{
+    showProgress(context, 'Resetting Streak...', true);
+    await Firestore.instance
+        .collection('users')
+        .document(MyAppState.currentUser.userId)
+        .updateData({
+      'streak': 0,
+    }).then((value) {
+      setState(() {
+        streak=0;
+        MyAppState.currentUser.streak = 0;
+      });
+    });
+    Navigator.pop(context);
+    hideProgress();
+  }
+
+  Future<void> _showMyDialog() async {
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: true, // user must tap button!
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Are you sure you wan\'t to reset your streak?'),
+          actions: <Widget>[
+            TextButton(
+              child: Text('Yes'),
+              onPressed: () async{
+                onTapReset();
+              },
+            ),
+            TextButton(
+              child: Text('No'),
+              onPressed: () async{
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
   }
 }
 
